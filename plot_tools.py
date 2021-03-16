f89c687d0ecbae17b97bbb2738f709dcf4707d1f
paulocv <paulo.pc.vs@gmail.com>
2021-03-16 15:53:47 -0300

    New add_arrow function. Rearrange imports.

--- plot_tools.py
+++ plot_tools.py
@@ -1,12 +1,14 @@
 """Tools for plot styling - creation of style sheets."""
 
+from cycler import cycler
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import matplotlib.font_manager
-from cycler import cycler
+import numpy as np
 import os
+
 from functools import reduce
-# import sys
+
 from toolbox.file_tools import SEP, write_config_string, list_to_csv
 
 _STD_USETEX = False
@@ -324,3 +326,39 @@ def set_product_prop_cycle(**props):
 def get_available_font_names():
     """Returns a list with fonts currently recognized by matplotlib"""
     return [f.name for f in mpl.font_manager.fontManager.ttflist]
+
+
+def add_arrow(line, position=None, direction='right', size=15, color=None):
+    """
+    By users Eric and thomas from stackoverflow
+    https://stackoverflow.com/questions/34017866/arrow-on-a-line-plot-with-matplotlib
+
+    Add an arrow to a line, showing the "direction" of the plot (when it makes sense).
+
+    line:       Line2D object
+    position:   x-position of the arrow. If None, mean of xdata is taken
+    direction:  'left' or 'right'
+    size:       size of the arrow in fontsize points
+    color:      if None, line color is taken.
+    """
+    if color is None:
+        color = line.get_color()
+
+    xdata = line.get_xdata()
+    ydata = line.get_ydata()
+
+    if position is None:
+        position = xdata.mean()
+    # find closest index
+    start_ind = np.argmin(np.absolute(xdata - position))
+    if direction == 'right':
+        end_ind = start_ind + 1
+    else:
+        end_ind = start_ind - 1
+
+    line.axes.annotate('',
+        xytext=(xdata[start_ind], ydata[start_ind]),
+        xy=(xdata[end_ind], ydata[end_ind]),
+        arrowprops=dict(arrowstyle="->", color=color),
+        size=size
+    )
